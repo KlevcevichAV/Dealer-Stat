cfe00c8c53ba96352af3cc6661a8a033eb6c345b
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-10-29 18:37:53 +0300

    feat: created class Dealer

--- src/main/java/com/dealerstat/model/Dealer.java
+++ src/main/java/com/dealerstat/model/Dealer.java
@@ -1,4 +1,74 @@
 package com.dealerstat.model;
 
 public class Dealer {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String created_at;
+    private Role role;
+
+    public Dealer(int id, String firstName, String lastName, String email, String password, String created_at) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.created_at = created_at;
+        this.role = Role.DEALER;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getRole(){
+        return role.name();
+    }
 }
