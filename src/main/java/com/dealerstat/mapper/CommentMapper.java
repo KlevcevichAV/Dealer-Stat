d1d93e06f68159a76761062ee6eefbfdbf845cae
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-11-01 17:50:03 +0300

    feat: added CommentMapper

--- src/main/java/com/dealerstat/mapper/CommentMapper.java
+++ src/main/java/com/dealerstat/mapper/CommentMapper.java
@@ -1,4 +1,20 @@
 package com.dealerstat.mapper;
 
-public class CommentMapper {
+import com.dealerstat.entity.Comment;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CommentMapper implements RowMapper<CommentMapper> {
+    @Override
+    public CommentMapper mapRow(ResultSet resultSet, int i) throws SQLException {
+        Comment comment = new Comment();
+        comment.setId(resultSet.getInt("id"));
+        comment.setMessage(resultSet.getString("message"));
+        comment.setUserId(resultSet.getInt("user_id"));
+        comment.setCreatedAt(resultSet.getString("created_at"));
+        comment.setApproved("true".equals(resultSet.getString("approved")));
+        return null;
+    }
 }
