3a75aac14bec7ac2f89d41585e9e7d802b490556
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-11-01 17:50:17 +0300

    feat: added GameMapper

--- src/main/java/com/dealerstat/mapper/GameMapper.java
+++ src/main/java/com/dealerstat/mapper/GameMapper.java
@@ -1,4 +1,17 @@
 package com.dealerstat.mapper;
 
-public class GameMapper {
+import com.dealerstat.entity.Game;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GameMapper implements RowMapper<Game> {
+    @Override
+    public Game mapRow(ResultSet resultSet, int i) throws SQLException {
+        Game game = new Game();
+        game.setId(resultSet.getInt("id"));
+        game.setName(resultSet.getString("name"));
+        return game;
+    }
 }
