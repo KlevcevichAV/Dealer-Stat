c97ed47c1c1a8b3a76bc535eb56ebfebf64484ed
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-11-01 17:46:51 +0300

    feat: added getting user and users

--- src/main/java/com/dealerstat/dao/UserDao.java
+++ src/main/java/com/dealerstat/dao/UserDao.java
@@ -1,4 +1,28 @@
 package com.dealerstat.dao;
 
+import com.dealerstat.entity.User;
+import com.dealerstat.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
 public class UserDao {
+
+    public final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<User> getUsers(){
+        String sql = "SELECT * FROM users";
+        return jdbcTemplate.query(sql, new UserMapper());
+    }
+
+    public User getUser(int id){
+        String sql = "SELECT * FROM users WHERE id=?";
+        return jdbcTemplate.queryForObject(sql, new UserMapper(), id);
+    }
 }
