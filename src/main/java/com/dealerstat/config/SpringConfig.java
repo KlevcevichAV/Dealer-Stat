2f3652b7074e3004255575eb0ed22a30a6f0e5ad
KlevcevichAV <alexandr.klevcevich@gmail.com>
2020-11-02 17:52:32 +0300

    feat: added getting users

--- src/main/java/com/dealerstat/config/SpringConfig.java
+++ src/main/java/com/dealerstat/config/SpringConfig.java
@@ -1,4 +1,41 @@
 package com.dealerstat.config;
 
+import com.dealerstat.dao.UserDao;
+import com.dealerstat.entity.User;
+import com.dealerstat.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class SpringConfig {
+
+    @Bean
+    public JdbcTemplate getJDBCTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/dealer_stat?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123");
+        return dataSource;
+    }
+
+    @Bean
+    public UserDao getUserDao(){
+        return new UserDao(getJDBCTemplate());
+    }
+
+    @Bean
+    public UserService userService1(){
+        return new UserService();
+    }
+
 }
--- src/main/java/com/dealerstat/service/UserService.java
+++ src/main/java/com/dealerstat/service/UserService.java
@@ -1,4 +1,23 @@
 package com.dealerstat.service;
 
+import com.dealerstat.dao.UserDao;
+import com.dealerstat.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    public UserDao userDao;
+
+    public List<User> getUsers() {
+        return userDao.getUsers();
+    }
+
+    public User getUser(int id) {
+        return userDao.getUser(id);
+    }
 }
